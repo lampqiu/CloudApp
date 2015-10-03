dc5dfe26e1c38dbdaf09bcd99ad2bcb82bb2ca30
lampqiu <lampqiu@sina.com>
2015-10-03 13:39:31 +0800

    Inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CloudApp::Application.config.secret_key_base = 'b093ff66d6292ad63a4d484955f90684cb0bfe7a9e29da872ffd68f56038b9d9833c8616686d9fdbac54620bf42509756744bc89c72597db9ab45bf0f5f59070'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CloudApp::Application.config.session_store :cookie_store, key: '_cloudApp_session'
